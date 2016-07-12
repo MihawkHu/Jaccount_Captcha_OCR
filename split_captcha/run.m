7e774b9dfbd56bd1790f32a7205754156d2a4b18
MihawkHu <MihawkHu@gmail.com>
2016-07-12 11:48:50 +0800

    resize splited captcha

--- split_captcha/run.m
+++ split_captcha/run.m
@@ -19,8 +19,17 @@ for i = 0 : 999
     end
 end
 
-%  TODO
 % resize them to the same size
+% save in ./captcha_splited_2/
 % max_len = 30
 % max_wid = 19
-
+for i = 0 : 4436
+    filePath = strcat("./captcha_splited/", num2str(i), ".bmp");
+    img = imread(filePath);
+    row = size(img, 1);
+    col = size(img, 2);
+    img_new = [255 * ones(floor((20 - row) / 2), 30); 255 * ones(row, floor((30 - col) / 2)), img, 255 * ones(row, ceil((30 - col) / 2)); 255 * ones(ceil((20 - row) / 2), 30)];
+    
+    save_path = strcat("./captcha_splited_resized/", num2str(i), ".bmp");
+    imwrite(img_new, save_path);
+end    
